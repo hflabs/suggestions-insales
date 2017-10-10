75ac5b9b93e689468bba81458a813ee725664ea7
Anton Zhiyanov <m@antonz.ru>
2017-10-10 18:15:18 +0300

    Подсказки по адресу и email

--- init.js
+++ init.js
@@ -17,6 +17,21 @@
     $field.parent().removeClass("co-input--empty_nested");
   }
 
+  function pass() {}
+
+  function init(id, type, showFunc, clearFunc, options) {
+    var plugin = $("#" + id).suggestions({
+      token: TOKEN,
+      partner: PARTNER,
+      type: type,
+      onSelect: showFunc,
+      onSelectNothing: clearFunc
+    }).suggestions();
+    if (options) {
+      plugin.setOptions(options);
+    }
+  }
+
   function clearParty() {
     clearField("client_juridical_address");
     clearField("client_inn");
@@ -34,16 +49,6 @@
     showField("client_kpp", party.kpp);
     showField("client_ogrn", party.ogrn);
   }
-
-  function initParty() {
-    $("#client_name").suggestions({
-      token: TOKEN,
-      partner: PARTNER,
-      type: "PARTY",
-      onSelect: showParty,
-      onSelectNothing: clearParty
-    });
-  }
   
   function clearBank() {
     clearField("client_bik");
@@ -55,20 +60,21 @@
     showField("client_bik", bank.bic);
     showField("client_correspondent_account", bank.correspondent_account);
   }
-
-  function initBank() {
-    $("#client_bank_name").suggestions({
-      token: TOKEN,
-      partner: PARTNER,
-      type: "BANK",
-      onSelect: showBank,
-      onSelectNothing: clearBank
-    });
+  
+  function clearAddress() {
+    clearField("shipping_address_zip");
+  }
+  
+  function showAddress(suggestion) {
+    var address = suggestion.data;
+    showField("shipping_address_zip", address.postal_code);
   }
   
   $(function () {
-    initParty();
-    initBank();
+    init("client_name", "PARTY", showParty, clearParty);
+    init("client_bank_name", "BANK", showBank, clearBank);
+    init("shipping_address_address", "ADDRESS", showAddress, clearAddress);
+    init("client_email", "EMAIL", pass, pass, { suggest_local: false });
   });
     
 }(window.jQuery, window.DADATA_TOKEN))
